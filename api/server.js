85f0bff3dd35f7120cc80f221540ff21a3a4f2fd
marcos <marcosmartinz176@gmail.com>
2022-04-11 17:47:01 -0400

    working on getting the first end point working

--- api/server.js
+++ api/server.js
@@ -1,3 +1,32 @@
-// BUILD YOUR SERVER HERE
+const express = require("express");
+const User = require("./users/model");
+const server = express();
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get("/api/users", (req, res) => {
+  User.find()
+    .then((users) => {
+      throw new Error("arghhh!!!!!");
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Users not found",
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+});
+
+server.use("*", (req, res) => {
+  res.status(404).json({
+    message: "not found",
+  });
+});
+
+// server.post("./api/users", (req, res) => {
+//   let addUser = req.body;
+//   Users.create(addUser).then((addUser) => {
+//     res.status(201).json(addUser);
+//   });
+// });
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
